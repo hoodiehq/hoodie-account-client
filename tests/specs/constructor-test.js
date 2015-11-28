4893afe0b4e4045544a819a79421188d61b2b39f
Gregor Martynus <gregor@martynus.net>
2015-11-28 16:25:53 -0500

    refactor: use t.end() for sync tests

--- tests/specs/constructor-test.js
+++ tests/specs/constructor-test.js
@@ -3,33 +3,33 @@ var test = require('tape')
 var Account = require('../../index')
 
 test('new Account(options)', function (t) {
-  t.plan(1)
-
   var account = new Account({
     url: 'http://localhost:3000/session/account'
   })
 
   t.is(typeof account, 'object', 'Account is a constructor')
+
+  t.end()
 })
 
 test('Account(options) w/o new', function (t) {
-  t.plan(1)
-
   var account = Account({
     url: 'http://localhost:3000/session/account'
   })
 
   t.is(typeof account, 'object', 'Account is a constructor')
+
+  t.end()
 })
 
 test('new Account() w/o options', function (t) {
-  t.plan(1)
-
   t.throws(Account, 'throws error')
+
+  t.end()
 })
 
 test('new Account() w/o options.url', function (t) {
-  t.plan(1)
-
   t.throws(Account.bind(null, { validate: function () {} }), 'throws error')
+
+  t.end()
 })
