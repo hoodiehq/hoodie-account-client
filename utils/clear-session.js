2312232a1386405ecd744801f4ed273a59862f5c
Gregor Martynus <gregor@martynus.net>
2015-11-28 21:03:00 -0500

    refactor: remove obsolete state.account & state.profile lines

--- utils/clear-session.js
+++ utils/clear-session.js
@@ -5,6 +5,4 @@ var localStorageWrapper = require('humble-localstorage')
 function clearSession (state) {
   localStorageWrapper.removeItem(state.cacheKey)
   delete state.session
-  delete state.account
-  delete state.profile
 }
