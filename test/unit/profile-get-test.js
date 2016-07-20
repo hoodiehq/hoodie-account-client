5e6f0f571c9d890974471d4abbc2aca8620d6887
Gregor <gregor@martynus.net>
2016-07-20 12:22:08 -0700

    test: account.profile.get() returns undefined when signed out

--- test/unit/profile-get-test.js
+++ test/unit/profile-get-test.js
@@ -6,6 +6,7 @@ var internals = get.internals
 
 test('profileGet', function (t) {
   simple.mock(internals, 'getProperties').returnWith('foo')
+  simple.mock(internals, 'isSignedIn').returnWith(true)
   var result = get({
     account: {
       profile: 'profile'
@@ -16,7 +17,7 @@ test('profileGet', function (t) {
     'profile',
     'path'
   ], 'calls utils.getProperties with "profile" basepath')
-  t.is(result, 'foo', 'returns result of fetchProperties')
+  t.is(result, 'foo', 'returns result of getProperties')
 
   simple.restore()
   t.end()
@@ -24,6 +25,7 @@ test('profileGet', function (t) {
 
 test('profileGet with empty profile', function (t) {
   simple.mock(internals, 'getProperties').returnWith(undefined)
+  simple.mock(internals, 'isSignedIn').returnWith(true)
   var result = get({
     account: {}
   })
@@ -32,8 +34,18 @@ test('profileGet with empty profile', function (t) {
     undefined,
     undefined
   ], 'calls utils.getProperties with "profile" basepath')
-  t.same(result, {}, 'returns result of fetchProperties')
+  t.same(result, {}, 'returns result of getProperties')
 
   simple.restore()
+  t.end()
+})
+
+test('profileGet when signed out', function (t) {
+  simple.mock(internals, 'isSignedIn').returnWith(false)
+  var result = get('internal state')
+
+  t.deepEqual(internals.isSignedIn.lastCall.args, ['internal state'], 'calls utils.isSignedIn with internal state')
+  t.same(result, undefined, 'returns undefined')
+
   t.end()
 })
