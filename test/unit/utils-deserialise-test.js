69cd117f64435921619c8018e7fcd52a03e9a9f3
Gregor <gregor@martynus.net>
2017-03-05 12:48:37 -0800

    chore: remove admin account client code & tests
    moved into `@hoodie/admin-client` via https://github.com/hoodiehq/hoodie-admin-client/pull/14

--- test/unit/utils-deserialise-test.js
+++ test/unit/utils-deserialise-test.js
@@ -7,12 +7,6 @@ var options = {
 }
 var signInResponse = require('../fixtures/signin.json')
 var signUpResponse = require('../fixtures/signup.json')
-var accountsWithProfileResponse = require('../fixtures/accounts-with-profile.json')
-var accountsWithProfileReturn = require('../fixtures/accounts-with-profile-return.json')
-var sessionsWithProfile = require('../fixtures/sessions-with-profile.json')
-var sessionsWithProfileReturn = require('../fixtures/sessions-with-profile-return.json')
-var accounts = require('../fixtures/accounts-with-missing-includes.json')
-var accountsReturn = require('../fixtures/accounts-return.json')
 
 test('throws error on non-JSON API response', function (t) {
   t.throws(deserialise.bind(null, {}, options), 'throws an error')
@@ -45,34 +39,6 @@ test('deserialise sign in response', function (t) {
       username: 'chicken@docs.com'
     }
   }, 'returns correct data')
-
-  t.end()
-})
-
-test('accounts with profile response', function (t) {
-  var data = deserialise(accountsWithProfileResponse, {
-    include: 'profile'
-  })
-
-  t.deepEqual(data, accountsWithProfileReturn, 'returns right data')
-
-  t.end()
-})
-
-test('Session with account->profile', function (t) {
-  var data = deserialise(sessionsWithProfile, {
-    include: 'account.profile'
-  })
-
-  t.deepEqual(data, sessionsWithProfileReturn, 'returns right data')
-
-  t.end()
-})
-
-test('accounts with missing includes', function (t) {
-  var data = deserialise(accounts, options)
-
-  t.deepEqual(data, accountsReturn, 'returns right data')
 
   t.end()
 })
